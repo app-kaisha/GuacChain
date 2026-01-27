72f5e8f614e7571a06b1033efa26beba7c6894a3
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-27 19:52:53 +0000

    Initial Commit

---
+++ GuacChain/GuacChainApp.swift
@@ -0,0 +1,18 @@
+//
+//  GuacChainApp.swift
+//  GuacChain
+//
+//  Created by app-kaihatsusha on 27/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct GuacChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
